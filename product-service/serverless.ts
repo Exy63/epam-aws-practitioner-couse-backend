343aefffc70c654a517d017a08a8e5be842ee862
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-09-29 11:37:19 +0300

    chore: add region to sls config

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -1,6 +1,7 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from '@serverless/typescript'
 
-import hello from '@functions/hello';
+import hello from '@functions/hello'
+import getProductList from '@functions/getProductList'
 
 const serverlessConfiguration: AWS = {
   service: 'product-service',
@@ -9,6 +10,8 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    stage: 'dev',
+    region: 'eu-west-1',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -19,7 +22,7 @@ const serverlessConfiguration: AWS = {
     },
   },
   // import the function via paths
-  functions: { hello },
+  functions: { hello, getProductList },
   package: { individually: true },
   custom: {
     esbuild: {
@@ -33,6 +36,6 @@ const serverlessConfiguration: AWS = {
       concurrency: 10,
     },
   },
-};
+}
 
-module.exports = serverlessConfiguration;
+module.exports = serverlessConfiguration
