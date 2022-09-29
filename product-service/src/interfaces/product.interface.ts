0c8dc203d01214de7e307f29cc7571b018fea8b2
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-09-29 11:30:02 +0300

    feat: create product.service

---
+++ product-service/src/interfaces/product.interface.ts
@@ -0,0 +1,5 @@
+export interface ProductI {
+  uuid: string
+  productName: string
+  price: number
+}
---
+++ product-service/src/product.service.ts
@@ -0,0 +1,28 @@
+import { ProductI } from './interfaces/product.interface'
+
+class ProductService {
+  private products: ProductI[] = [
+    {
+      uuid: '0dff3aa9-8ea9-4098-9ce0-921771c9d167',
+      productName: 'Pen',
+      price: 2,
+    },
+    {
+      uuid: '49644adf-56eb-4d50-b214-a5cc233510c5',
+      productName: 'Pencil',
+      price: 1,
+    },
+    {
+      uuid: '8f260efa-a54e-4522-903d-16b0686c2d34',
+      productName: 'Book',
+      price: 5,
+    },
+  ]
+
+  public getProducts(): ProductI[] {
+    const products = this.products
+    return products
+  }
+}
+
+export default new ProductService()
