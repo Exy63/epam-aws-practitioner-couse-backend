e8e5e75b3dcf594383da306abe4b1e0e98369c22
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-09-30 01:21:08 +0300

    docs: add enpoint docs to hello lambda

--- product-service/src/functions/hello/handler.ts
+++ product-service/src/functions/hello/handler.ts
@@ -2,9 +2,14 @@ import { formatJSONResponse } from '@libs/api-gateway'
 import { middyfy } from '@libs/lambda'
 
 const hello = async () => {
-    return formatJSONResponse({
-      message: `Hello and welcome to my API!`,
-    })
-  }
+  return formatJSONResponse({
+    message: `Hello and welcome to my API!`,
+    endpoints: {
+      'GET - /products': 'Gives you a full array of products',
+      'GET - /products/${id}':
+        'Gives you one searched product from an array of products',
+    },
+  })
+}
 
 export const main = middyfy(hello)
