00680c4d0ee968bf3bfa1b54c2616e775a3ea120
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-10-03 18:09:06 +0300

    chore: add role statements to sls config

--- product-service/src/db/tools.ts
+++ product-service/src/db/tools.ts
@@ -4,21 +4,30 @@ AWS.config.update({ region: 'eu-west-1' })
 const dynamo = new AWS.DynamoDB.DocumentClient()
 
 export const findAll = async (TableName: TableName) => {
-  return await dynamo
-    .scan({
-      TableName,
-    })
-    .promise()
+  return (
+    await dynamo
+      .scan({
+        TableName,
+      })
+      .promise()
+  )?.Items
 }
 
-export const findById = async (id: string, TableName: TableName) => {
-  return await dynamo
-    .query({
-      TableName,
-      KeyConditionExpression: 'id = :id',
-      ExpressionAttributeValues: { ':id': id },
-    })
-    .promise()
+export const findByKey = async (
+  options: { keyName?: string; value: any },
+  TableName: TableName
+) => {
+  const { value } = options
+  const keyName = options?.keyName || 'id'
+  return (
+    await dynamo
+      .query({
+        TableName,
+        KeyConditionExpression: `${keyName} = :id`,
+        ExpressionAttributeValues: { ':id': value },
+      })
+      .promise()
+  )?.Items
 }
 
 export const insert = async (Item, TableName: TableName) => {
