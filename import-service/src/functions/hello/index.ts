3c3db87f9791e21c3e46dd673a35b5be3cef3dea
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-10-04 19:49:04 +0300

    init: import-service

---
+++ import-service/src/functions/hello/index.ts
@@ -0,0 +1,13 @@
+import { handlerPath } from '@libs/handler-resolver'
+
+export const hello = {
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      http: {
+        method: 'post',
+        path: 'hello',
+      },
+    },
+  ],
+}
