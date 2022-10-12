7fd92cf40c1522a77eeefcbe3cbd395eda18fa79
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-10-13 00:38:22 +0300

    chore: update importProductsFile for FE calls

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,6 +1,5 @@
 import {
   formatJSONErrorResponse,
-  formatJSONResponse,
   ValidatedEventAPIGatewayProxyEvent,
 } from '@libs/api-gateway'
 import { middyfy } from '@libs/lambda'
@@ -8,28 +7,32 @@ import schema from './schema'
 
 const AWS = require('aws-sdk')
 const s3 = new AWS.S3({ region: 'eu-west-1' })
+
 const BUCKET = 'superstore-import'
+const FRONTEND_URL = 'https://d33fy6chog9gs4.cloudfront.net'
 
 const importProductsFile: ValidatedEventAPIGatewayProxyEvent<
   typeof schema
 > = async (event) => {
   const { name } = event.queryStringParameters
-  console.log('Lambda importProductsFile is invoked! fileName: ' + name)
+  console.log('Lambda importProductsFile is invoked! File name: ' + name)
 
   try {
     const params = {
       Bucket: BUCKET,
-      Key: `uploaded/${name}.csv`,
+      Key: `uploaded/${name}`,
       Expires: 60 * 5, // 5 minutes
-      ContentType: 'text/csv',
     }
 
     const url = await s3.getSignedUrl('putObject', params)
 
-    return formatJSONResponse({
-      result: `You are good, here is your URL to upload a .csv file! Please, make sure that you send PUT request with {"Content-Type": "text/csv"} in headers`,
-      url,
-    })
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': FRONTEND_URL,
+      },
+      body: url,
+    }
   } catch (e) {
     return formatJSONErrorResponse(e, 500)
   }
