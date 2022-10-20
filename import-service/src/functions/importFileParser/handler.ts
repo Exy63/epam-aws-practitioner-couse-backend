2487b67b54e636c39f6138a2b546f6b01b1d80d1
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-10-20 23:26:56 +0300

    feat: implement importFileParser

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -1,23 +1,23 @@
-import {
-  formatJSONErrorResponse,
-  ValidatedEventAPIGatewayProxyEvent,
-} from '@libs/api-gateway'
 import { middyfy } from '@libs/lambda'
-import schema from './schema'
+const csv = require('csv-parser')
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3({ region: 'eu-west-1' })
 
-const importFileParser: ValidatedEventAPIGatewayProxyEvent<
-  typeof schema
-> = async (event) => {
-  console.log('Lambda importFileParser is invoked!', event)
+const BUCKET = 'superstore-import'
 
-  try {
-    return {
-      statusCode: 200,
-      body: 'Hello from importFileParser!',
-    }
-  } catch (e) {
-    return formatJSONErrorResponse(e, 500)
-  }
+const importFileParser = async (event: any) => {
+  console.log('Lambda importFileParser is invoked! Event: ', event)
+  const records = event.Records[0]
+
+  const params = { Bucket: BUCKET, Key: records.s3.object.key }
+
+  const readStream = s3.getObject(params).createReadStream()
+
+  await new Promise(() => {
+    readStream.pipe(csv()).on('data', (chunk) => {
+      console.log('🚀 chunk ->', chunk)
+    })
+  })
 }
 
 export const main = middyfy(importFileParser)
