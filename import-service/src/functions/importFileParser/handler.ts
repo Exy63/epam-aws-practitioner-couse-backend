4fc3cf611f7b4125f461dcb143f02b4d0ef36931
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-10-21 03:40:50 +0300

    chore: add try/catch to importFileParser

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -1,3 +1,4 @@
+import { formatJSONErrorResponse } from '@libs/api-gateway'
 import { middyfy } from '@libs/lambda'
 const csv = require('csv-parser')
 const AWS = require('aws-sdk')
@@ -8,46 +9,50 @@ const importFileParser = async (event: any) => {
   const s3 = new AWS.S3({ region: 'eu-west-1' })
   console.log('Lambda importFileParser is invoked! Event: ', event)
 
-  const csvKey = event.Records[0].s3.object.key
-  const [oldPrefix, fileName] = csvKey.split('/')
-  const newPrefix = 'parsed'
+  try {
+    const csvKey = event.Records[0].s3.object.key
+    const [oldPrefix, fileName] = csvKey.split('/')
+    const newPrefix = 'parsed'
 
-  const paramsToRead = { Bucket: BUCKET, Key: `${oldPrefix}/${fileName}` }
-  const paramsToWrite = {
-    Bucket: BUCKET,
-    CopySource: `${BUCKET}/${oldPrefix}/${fileName}`,
-    Key: `${newPrefix}/${fileName}`,
-  }
+    const paramsToRead = { Bucket: BUCKET, Key: `${oldPrefix}/${fileName}` }
+    const paramsToWrite = {
+      Bucket: BUCKET,
+      CopySource: `${BUCKET}/${oldPrefix}/${fileName}`,
+      Key: `${newPrefix}/${fileName}`,
+    }
 
-  const readStream = s3.getObject(paramsToRead).createReadStream()
-  await new Promise<void>((resolve, reject) => {
-    readStream
-      .pipe(csv())
-      .on('data', (chunk) => {
-        console.log('🚀 chunk ->', chunk)
-      })
-      .on('error', (err) => {
-        reject(err)
-      })
-      .on('end', () => {
-        resolve()
-      })
-  })
-
-  const isCopied = await new Promise<any>((resolve, reject) => {
-    s3.copyObject(paramsToWrite, (err, data) => {
-      if (err) reject(err) // error
-      else resolve(data) // success
+    const readStream = s3.getObject(paramsToRead).createReadStream()
+    await new Promise<void>((resolve, reject) => {
+      readStream
+        .pipe(csv())
+        .on('data', (chunk) => {
+          console.log('🚀 chunk ->', chunk)
+        })
+        .on('error', (err) => {
+          reject(err)
+        })
+        .on('end', () => {
+          resolve()
+        })
     })
-  })
 
-  if (isCopied) {
-    new Promise<void>((resolve, reject) => {
-      s3.deleteObject(paramsToRead, (err, data) => {
+    const isCopied = await new Promise<any>((resolve, reject) => {
+      s3.copyObject(paramsToWrite, (err, data) => {
         if (err) reject(err) // error
         else resolve(data) // success
       })
     })
+
+    if (isCopied) {
+      new Promise<void>((resolve, reject) => {
+        s3.deleteObject(paramsToRead, (err, data) => {
+          if (err) reject(err) // error
+          else resolve(data) // success
+        })
+      })
+    }
+  } catch (e) {
+    return formatJSONErrorResponse('Bad Request', 400)
   }
 }
 
