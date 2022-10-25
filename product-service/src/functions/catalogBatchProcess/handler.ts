c043db032b25814ec355d546c45cf6c901843e06
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-10-25 23:27:16 +0300

    chore: init catalogBatchProcess

--- product-service/src/functions/catalogBatchProcess/handler.ts
+++ product-service/src/functions/catalogBatchProcess/handler.ts
@@ -1,17 +1,42 @@
-import {
-  formatJSONErrorResponse,
-  ValidatedEventAPIGatewayProxyEvent,
-} from '@libs/api-gateway'
+import { formatJSONErrorResponse } from '@libs/api-gateway'
 import { middyfy } from '@libs/lambda'
-import schema from './schema'
+import { APIGatewayProxyEvent } from 'aws-lambda'
 
+interface CatalogBatchProcessEventI extends APIGatewayProxyEvent {
+  Records: {
+    body: string
+  }[]
+}
 
-const catalogBatchProcess: ValidatedEventAPIGatewayProxyEvent<
-  typeof schema
-> = async (event) => {
-  console.log('Lambda catalogBatchProcess is invoked! File name: ' + event)
+interface ParsedBody {
+  price: number
+  count: number
+  description?: string
+  title: string
+}
 
+const catalogBatchProcess = async (event: CatalogBatchProcessEventI) => {
+  console.log('Lambda catalogBatchProcess is invoked! Event: ', event)
   try {
+    const validatedProducts = event.Records.map(({ body }) => {
+      const parsedBody: ParsedBody = JSON.parse(body)
+      if (
+        parsedBody.price > 0 &&
+        parsedBody.count > 0 &&
+        parsedBody.title.length > 0
+      ) {
+        return parsedBody
+      }
+    })
+    console.log("🚀 ~  validatedProducts", validatedProducts)
+
+    // validatedProducts.forEach(async (product) => {
+    //   const { count, ...productInfo } = product
+    //   console.log("🚀 ~ file: handler.ts ~ line 37 ~ validatedProducts.forEach ~ productInfo", productInfo)
+
+    //   const productId = await productService.createProduct(productInfo)
+    //   console.log("🚀 ~ file: handler.ts ~ line 39 ~ validatedProducts.forEach ~ productId", productId)
+    // })
   } catch (e) {
     return formatJSONErrorResponse(e, 500)
   }
