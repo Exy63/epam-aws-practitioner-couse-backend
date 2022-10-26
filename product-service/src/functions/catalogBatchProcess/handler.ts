3ea0604ea3abed2f7b18aa1b627e79db565de7ba
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-10-26 14:30:11 +0300

    feat: add db processing to catalogBatchProcess

--- product-service/src/functions/catalogBatchProcess/handler.ts
+++ product-service/src/functions/catalogBatchProcess/handler.ts
@@ -1,45 +1,69 @@
-import { formatJSONErrorResponse } from '@libs/api-gateway'
-import { middyfy } from '@libs/lambda'
-import { APIGatewayProxyEvent } from 'aws-lambda'
+import { formatJSONErrorResponse } from "@libs/api-gateway";
+import { middyfy } from "@libs/lambda";
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { v4 } from "uuid";
+import { dynamo } from "@db/tools";
+import { PRODUCTS_TABLE_NAME, STOCKS_TABLE_NAME } from "@db/constants/table-name.constants";
 
 interface CatalogBatchProcessEventI extends APIGatewayProxyEvent {
   Records: {
-    body: string
-  }[]
+    body: string;
+  }[];
 }
 
 interface ParsedBody {
-  price: number
-  count: number
-  description?: string
-  title: string
+  price: number;
+  count: number;
+  description?: string;
+  title: string;
 }
 
 const catalogBatchProcess = async (event: CatalogBatchProcessEventI) => {
-  console.log('Lambda catalogBatchProcess is invoked! Event: ', event)
+  console.log("Lambda catalogBatchProcess is invoked! Event: ", event);
   try {
     const validatedProducts = event.Records.map(({ body }) => {
-      const parsedBody: ParsedBody = JSON.parse(body)
-      if (
+      const parsedBody: ParsedBody = JSON.parse(body);
+
+      const isRequestValid =
         parsedBody.price > 0 &&
         parsedBody.count > 0 &&
-        parsedBody.title.length > 0
-      ) {
-        return parsedBody
+        parsedBody.title.length > 0;
+
+      if (isRequestValid) {
+        return parsedBody;
       }
-    })
-    console.log("🚀 ~  validatedProducts", validatedProducts)
+    });
+    console.log("🚀 ~  validatedProducts", validatedProducts);
 
-    // validatedProducts.forEach(async (product) => {
-    //   const { count, ...productInfo } = product
-    //   console.log("🚀 ~ file: handler.ts ~ line 37 ~ validatedProducts.forEach ~ productInfo", productInfo)
+    const promises = [];
 
-    //   const productId = await productService.createProduct(productInfo)
-    //   console.log("🚀 ~ file: handler.ts ~ line 39 ~ validatedProducts.forEach ~ productId", productId)
-    // })
+    validatedProducts.forEach(async (product) => {
+      const { count, ...productInfo } = product;
+
+      const id = v4();
+
+      const productPromise = dynamo
+        .put({
+          TableName: PRODUCTS_TABLE_NAME,
+          Item: { ...productInfo, id },
+        })
+        .promise();
+
+      const stockPromise = dynamo
+        .put({
+          TableName: STOCKS_TABLE_NAME,
+          Item: { product_id: id, count },
+        })
+        .promise();
+
+      promises.push(productPromise);
+      promises.push(stockPromise);
+    });
+
+    await Promise.all(promises);
   } catch (e) {
-    return formatJSONErrorResponse(e, 500)
+    return formatJSONErrorResponse(e, 500);
   }
-}
+};
 
-export const main = middyfy(catalogBatchProcess)
+export const main = middyfy(catalogBatchProcess);
