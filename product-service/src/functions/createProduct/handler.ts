e2e45ed0e53e444c98620e5cd7a15902c51da06b
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-10-26 13:09:09 +0300

    chore: add error handling to create product

--- product-service/src/functions/createProduct/handler.ts
+++ product-service/src/functions/createProduct/handler.ts
@@ -2,47 +2,45 @@ import {
   formatJSONErrorResponse,
   formatJSONResponse,
   ValidatedEventAPIGatewayProxyEvent,
-} from '@libs/api-gateway'
-import { middyfy } from '@libs/lambda'
+} from "@libs/api-gateway";
+import { middyfy } from "@libs/lambda";
 
-import schema from './schema'
+import schema from "./schema";
 
-import productService from '../../product.service'
-import stockService from '../../stock.service'
-import { ProductI } from '@interfaces/product.interface'
+import productService from "../../product.service";
+import stockService from "../../stock.service";
+import { ProductI } from "@interfaces/product.interface";
 
 export const createProduct: ValidatedEventAPIGatewayProxyEvent<
   typeof schema
 > = async (event) => {
   try {
-    console.log(
-      'Lambda createProduct is invoked! Body: ' + JSON.stringify(event.body)
-    )
+    console.log("Lambda createProduct is invoked! Event: ", event);
 
-    const count = event.body.count as number
+    const count = event.body?.count as number;
     const product = {
-      title: event.body.title,
+      title: event.body?.title,
       description: event.body?.description,
-      price: event.body.price,
-    } as ProductI
+      price: event.body?.price,
+    } as ProductI;
 
     if (!product.title || !product.price || !count) {
       return formatJSONErrorResponse(
         "Your request isn't valid. Please fill title, price and count",
         400
-      )
+      );
     }
 
-    const productId = await productService.createProduct(product)
+    const productId = await productService.createProduct(product);
 
-    await stockService.createStock({ product_id: productId, count })
+    await stockService.createStock({ product_id: productId, count });
 
     return formatJSONResponse({
       products: { id: productId, ...event.body },
-    })
+    });
   } catch (e) {
-    return formatJSONErrorResponse(e, 500)
+    return formatJSONErrorResponse(e, 500);
   }
-}
+};
 
-export const main = middyfy(createProduct)
+export const main = middyfy(createProduct);
