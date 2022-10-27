b1f2ff302549bed7fd704b7634874c40be66de44
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-10-27 15:48:54 +0300

    feat: create sns topic and subscription

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -1,62 +1,51 @@
-import type { AWS } from '@serverless/typescript'
+import type { AWS } from "@serverless/typescript";
 
-import * as functions from '@functions'
-import dbConfig from '@db/config'
+import * as functions from "@functions";
+import dbConfig from "@db/config";
 
 const serverlessConfiguration: AWS = {
-  service: 'product-service',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  service: "product-service",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
-    stage: 'dev',
-    region: 'eu-west-1',
+    name: "aws",
+    runtime: "nodejs14.x",
+    stage: "dev",
+    region: "eu-west-1",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
-    iamRoleStatements: [
-      {
-        Effect: 'Allow',
-        Action: 'dynamodb:Scan',
-        Resource: { 'Fn::GetAtt': ['ProductsTable', 'Arn'] },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: "Allow",
+            Action: "dynamodb:*",
+            Resource: { "Fn::GetAtt": ["ProductsTable", "Arn"] },
+          },
+          {
+            Effect: "Allow",
+            Action: "dynamodb:*",
+            Resource: { "Fn::GetAtt": ["StocksTable", "Arn"] },
+          },
+          {
+            Effect: "Allow",
+            Action: "sqs:*",
+            Resource: { "Fn::GetAtt": ["CatalogItemsQueue", "Arn"] },
+          },
+          {
+            Effect: "Allow",
+            Action: "sns:*",
+            Resource: { Ref: "CreateProductTopic" },
+          },
+        ],
       },
-      {
-        Effect: 'Allow',
-        Action: 'dynamodb:Query',
-        Resource: { 'Fn::GetAtt': ['ProductsTable', 'Arn'] },
-      },
-      {
-        Effect: 'Allow',
-        Action: 'dynamodb:PutItem',
-        Resource: { 'Fn::GetAtt': ['ProductsTable', 'Arn'] },
-      },
-      {
-        Effect: 'Allow',
-        Action: 'dynamodb:Scan',
-        Resource: { 'Fn::GetAtt': ['StocksTable', 'Arn'] },
-      },
-      {
-        Effect: 'Allow',
-        Action: 'dynamodb:Query',
-        Resource: { 'Fn::GetAtt': ['StocksTable', 'Arn'] },
-      },
-      {
-        Effect: 'Allow',
-        Action: 'dynamodb:PutItem',
-        Resource: { 'Fn::GetAtt': ['StocksTable', 'Arn'] },
-      },
-      {
-        Effect: 'Allow',
-        Action: 'sqs:*',
-        Resource: { 'Fn::GetAtt': ['CatalogItemsQueue', 'Arn'] },
-      },
-    ],
+    },
   },
   // import the function via paths
   functions,
@@ -64,9 +53,23 @@ const serverlessConfiguration: AWS = {
     Resources: {
       ...dbConfig,
       CatalogItemsQueue: {
-        Type: 'AWS::SQS::Queue',
+        Type: "AWS::SQS::Queue",
         Properties: {
-          QueueName: 'CatalogItemsQueue',
+          QueueName: "CatalogItemsQueue",
+        },
+      },
+      CreateProductTopic: {
+        Type: "AWS::SNS::Topic",
+        Properties: {
+          TopicName: "CreateProductTopic",
+        },
+      },
+      CreateProductSubscription: {
+        Type: "AWS::SNS::Subscription",
+        Properties: {
+          Endpoint: "ilyaprikaz@gmail.com",
+          Protocol: "email",
+          TopicArn: { Ref: "CreateProductTopic" },
         },
       },
     },
@@ -77,13 +80,13 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
     },
   },
-}
+};
 
-module.exports = serverlessConfiguration
+module.exports = serverlessConfiguration;
