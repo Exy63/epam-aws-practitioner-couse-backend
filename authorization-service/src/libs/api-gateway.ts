d15adc9d052b89400ef1016d7825619ab3710c71
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-11-03 22:48:06 +0300

    chore: update formatJSONResponse

--- authorization-service/src/libs/api-gateway.ts
+++ authorization-service/src/libs/api-gateway.ts
@@ -4,9 +4,9 @@ import type { FromSchema } from "json-schema-to-ts";
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
-export const formatJSONResponse = (response: Record<string, unknown>) => {
+export const formatJSONResponse = (response: Record<string, unknown>, code = 200) => {
   return {
-    statusCode: 200,
-    body: JSON.stringify(response)
-  }
+    statusCode: code,
+    body: JSON.stringify(response),
+  };
 }
