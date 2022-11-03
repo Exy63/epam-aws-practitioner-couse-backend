59d421e8b3aaffc79d73815649670e09f19b9b10
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-11-04 02:56:33 +0300

    refactor: add types

--- authorization-service/src/functions/basicAuthorizer/index.ts
+++ authorization-service/src/functions/basicAuthorizer/index.ts
@@ -1,5 +1,6 @@
 import { handlerPath } from "@libs/handler-resolver";
+import { AWS } from "@serverless/typescript";
 
-export const basicAuthorizer = {
+export const basicAuthorizer: AWS["functions"]["k"] = {
   handler: `${handlerPath(__dirname)}/handler.main`,
 };
