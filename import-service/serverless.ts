0003d4caed03f86ddafa925eb4979bc60346aac9
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-11-03 23:11:35 +0300

    lint: apply prettier

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,44 +1,44 @@
-import type { AWS } from '@serverless/typescript'
+import type { AWS } from "@serverless/typescript";
 
-import * as functions from '@functions'
+import * as functions from "@functions";
 
 const serverlessConfiguration: AWS = {
-  service: 'import-service',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  service: "import-service",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
-    stage: 'dev',
-    region: 'eu-west-1',
+    name: "aws",
+    runtime: "nodejs14.x",
+    stage: "dev",
+    region: "eu-west-1",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
       SQS_URL:
-        'https://sqs.eu-west-1.amazonaws.com/203651338148/CatalogItemsQueue',
-      IMPORT_BUCKET_NAME: 'superstore-import',
+        "https://sqs.eu-west-1.amazonaws.com/203651338148/CatalogItemsQueue",
+      IMPORT_BUCKET_NAME: "superstore-import",
     },
     iam: {
       role: {
         statements: [
           {
-            Effect: 'Allow',
-            Action: 's3:ListBucket',
-            Resource: 'arn:aws:s3:::superstore-import',
+            Effect: "Allow",
+            Action: "s3:ListBucket",
+            Resource: "arn:aws:s3:::superstore-import",
           },
           {
-            Effect: 'Allow',
-            Action: 's3:*',
-            Resource: 'arn:aws:s3:::superstore-import/*',
+            Effect: "Allow",
+            Action: "s3:*",
+            Resource: "arn:aws:s3:::superstore-import/*",
           },
           {
-            Effect: 'Allow',
-            Action: 'sqs:*',
-            Resource: 'arn:aws:sqs:eu-west-1:203651338148:CatalogItemsQueue',
+            Effect: "Allow",
+            Action: "sqs:*",
+            Resource: "arn:aws:sqs:eu-west-1:203651338148:CatalogItemsQueue",
           },
         ],
       },
@@ -52,13 +52,13 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
     },
   },
-}
+};
 
-module.exports = serverlessConfiguration
+module.exports = serverlessConfiguration;
