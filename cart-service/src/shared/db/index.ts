ecd6c9d074e6dca9d19bdbc9ad693acb82e8e374
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-11-20 01:15:32 +0300

    chore: update db pool to client

--- cart-service/src/shared/db/index.ts
+++ cart-service/src/shared/db/index.ts
@@ -1,11 +1,13 @@
-import { Pool } from 'pg';
+import { Client } from 'pg';
 
 const { PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } = process.env;
 
-export const dbPool = new Pool({
+const dbOptions = {
   user: PG_USER,
   host: PG_HOST,
   database: PG_DATABASE,
   password: PG_PASSWORD,
-  port: PG_PORT,
-});
+  port: Number(PG_PORT),
+};
+
+export const dbClient = new Client(dbOptions);
--- cart-service/src/users/services/users.service.ts
+++ cart-service/src/users/services/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { dbPool } from '@db';
+import { dbClient } from '@db';
 import { throwError } from 'src/shared/helpers';
 import { v4 } from 'uuid';
 import bcrypt from 'bcryptjs';
@@ -8,10 +8,10 @@ import { User, userTableName, userTableValues } from '../models';
 
 @Injectable()
 export class UsersService {
-  private readonly db = dbPool;
-
   async findOne(userId: string): Promise<User> {
     try {
+      dbClient.connect();
+
       const query = `
       SELECT id, name, email 
       FROM ${userTableName} 
@@ -19,15 +19,19 @@ export class UsersService {
       ;`;
       const values = [userId];
 
-      const { rows: res } = await this.db.query(query, values);
-      return res;
+      const { rows: res } = await dbClient.query(query, values);
+      return res?.[0] as User;
     } catch (e) {
       throwError(e, 502);
+    } finally {
+      dbClient.end();
     }
   }
 
   async createOne(data: User): Promise<string> {
     try {
+      dbClient.connect();
+
       const { name, email } = data;
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(data.password, salt);
@@ -39,15 +43,13 @@ export class UsersService {
       `;
       const values = [id, name, email, hashedPassword];
 
-      await this.db.query(query, values);
+      await dbClient.query(query, values);
 
       return id;
     } catch (e) {
       throwError(e, 502);
+    } finally {
+      dbClient.end();
     }
-  }
-
-  test() {
-    console.log('HELLO! DASDKASKJDASJKDJHADL')
   }
 }
