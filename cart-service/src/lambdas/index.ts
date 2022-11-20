87489ff38d560fe7c31a864963f2c829e48af502
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-11-20 11:12:15 +0300

    feat: create order lambdas

--- cart-service/src/lambdas/index.ts
+++ cart-service/src/lambdas/index.ts
@@ -1,3 +1,7 @@
 // User
 export { createUser } from './user/create-user';
 export { getUser } from './user/get-user';
+// Order
+export { getOrder } from './order/get-order';
+export { createOrder } from './order/create-order';
+export { updateOrder } from './order/update-order';
